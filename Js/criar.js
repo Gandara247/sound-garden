d13384fdca89950d330754fd6e0f9110232b2001
Gandara <gustavogandarasilva@gmail.com>
2023-02-18 05:55:45 -0300

    feat: add scripts edit/delete/create...

--- Js/criar.js
+++ Js/criar.js
@@ -1,5 +1,5 @@
-import { eventPost } from "./utils/eventsPost.js";
-import { dateISO8601 } from "./utils/date.js";
+const urlApi = "https://soundgarden-api.vercel.app/events/"
+import { readForm } from "./utils/readForm.js";
 
 
 var fNewEvent = document.querySelector(".col-6");
@@ -9,37 +9,23 @@ const newEvent = {};
 fNewEvent.addEventListener("submit", function (event) {
     event.preventDefault();
 
-
     const inputs = fNewEvent.elements;
+    readForm(inputs, newEvent);
 
+    fetch(urlApi, {
+        method: "POST",
+        headers: { "Content-Type": "application/json", },
+        body: JSON.stringify(newEvent),
+    })
 
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].type !== "submit") {
-            if (!inputs[i].value) {
-                alert("Preencha todos os campos para cadastrar");
-                return;
-            }
-            switch (inputs[i].name) {
-                case "attractions":
-                    newEvent[inputs[i].name] = inputs[i].value.split(/\s*,\s*/);
+        .then((response) => response.json())
+        .then(() => {
+            window.location.replace("./admin.html");
+            alert("O evento foi criado com sucesso!!!");
+        })
+        .catch((error) => {
+            console.error
+            ("Erro no processamento junto ao servidor", error);
+        })
 
-                    break;
-                case "number_tickets":
-                    newEvent[inputs[i].name] = parseInt(inputs[i].value);
-                    break;
-
-                case "scheduled":
-                    let eventDate = inputs[i].value;
-                    let rule = /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}$/;
-                    if (!eventDate.match(rule)) {
-                        alert("formato de data e hora aceitos 00/00/0000 00:00.");
-                        return;
-                    }
-                    newEvent[inputs[i].name] = dateISO8601(eventDate);
-                    break;
-                default: newEvent[inputs[i].name] = inputs[i].value;
-            }
-        }
-    }
-    eventPost(newEvent)
 })
--- Js/edit.js
+++ Js/edit.js
@@ -1,87 +1,58 @@
-const urlApi = "https://soundgarden-api.vercel.app/events/:id"
+const urlApi = "https://soundgarden-api.vercel.app/events/"
+import { dataLocal } from "./utils/dataToLocal.js";
+import { readForm } from "./utils/readForm.js";
 
-function editEvents() {
-    let params = { method: "GET", redirect: "follow", };
-
-
-    fetch(urlApi, params)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let btnEdit = document.getElementsByClassName("btn btn-secondary")
-            btnEdit.textContext = data.id;
-            btnEdit.addEventListener("click", () => {
-                console.log(window.location.pathname)
-                window.location.href = "editar-evento.html"
-            })
-        })
-
-        .catch(error => {
-            console.log("error", error)
-        })
-
-}
-
-editEvents();
-
-function eventUpdate() {
-    let paramsUpdate = {
-        method: "PUT",
-        body: "row", redirect: "follow"
-    };
-    fetch(urlApi, paramsUpdate)
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(error => consol.log("error", error));
-}
-
-eventUpdate()
-
-
-
-
-const url = new URL(window.location.href);
-const searchId = new URLSearchParams(url.search).get("id");
 const fillForm = function (data) {
-    const { name, banner, attractions, description, tickets } = data;
+    const { name, poster, attractions, description, number_tickets } = data;
     document.querySelector("#nome").value = name;
-    document.querySelector("#banner").value = banner;
+    document.querySelector("#banner").value = poster;
     document.querySelector("#atracoes").value = attractions.join(",  ");
     document.querySelector("#descricao").value = description;
-    document.querySelector("#lotacao").value = tickets;
+    document.querySelector("#data").value = dataLocal(scheduled);
+    document.querySelector("#lotacao").value = number_tickets;
 }
 
-const editEvent = function (event) {
-    fetch(urlApi, {
-        method: "GET", redirect: "follow",
-        headers: { "Content-Type": "application/json" }
-    })
-
-        .then((response) => response.json())
-        .then((data) => fillForm(data))
-        .catch((error) => console.log("error", error))
+const idUrl = function () {
+    const url = new URL(window.location.href);
+    const searchId = new URLSearchParams(url.search);
+    const id = searchId.get("id");
+    return id;
 }
 
-editEvent()
+const id = idUrl();
 
-const btnEditEvent = document.querySelector("#btn-secondary");
-btnEditEvent.addEventListener("click", function (event) {
-    const eventName = document.querySelector("#nome").value
-    event.preventDefault();
-    fetch(urlApi, {
-        method: "GET", redirect: "follow",
-        headers: { "Content-Type": "application/json", }
-    })
 
-    .then((response) => response.text())
-    .then(result => console.log(result))
-    .then(() => {
-        window.location.replace("./editar-evento.html")
-        alert(eventName + "salvo")
-    })
-    .catch((error) => console.log("error", error))
+fetch(urlApi + id, {
+    method: "GET", redirect: "follow",
+    headers: { "Content-Type": "application/json", }
 })
+
+    .then((response) => response.json())
+    .then((data) => fillForm(data))
+    .catch((error) => console.log("error", error))
+
+var formEditEvent = document.querySelector(".col-6");
+const enventEdited = {};
+
+formEditEvent.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputs = formEditEvent.elements;
+    readForm(inputs, enventEdited);
+
+    fetch(urlApi + id, {
+        method: "PUT", redirect: "follow",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(enventEdited),
+    })
+
+        .then((response) => response.text())
+        .then(() => {
+            window.location.replace("./admin.html");
+            alert("Editado com sucesso!!!");
+        })
+        .catch ((error) => console.log("error", error));
+})
+
 
 
 
